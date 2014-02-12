2c1d606ff191b0715f8c558e829b5c8d8a8143ae
jekatigr <jekatigr@yandex.ru>
2014-02-12 22:02:08 +0400

    Добавлены getters&setters.

--- src/comparative_system/model/Algorithm.java
+++ src/comparative_system/model/Algorithm.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  */
 public class Algorithm {
     /** Индекс алгоритма в БД. */
-    private int id;
+    private long id;
     /** Имя алгоритма. */
     private String name;
     /** Метод вызова алгоритма. */
@@ -26,13 +26,24 @@ public class Algorithm {
     
     /**
      * Конструктор класса.
+     * @param name Имя алгоритма.
+     * @param mainMethod Метод вызова алгоритма.
+     * @param counterName Имя счетчика операций в сгенерированных кодах.
+     * @param codes Исходные и сгенерированные коды алгоритма.
+     */
+    public Algorithm(String name, String mainMethod, String counterName, ArrayList<Code> codes) {
+        this(-1, name, mainMethod, counterName, codes);
+    }
+    
+    /**
+     * Конструктор класса.
      * @param id Индекс алгоритма в БД.
      * @param name Имя алгоритма.
      * @param mainMethod Метод вызова алгоритма.
      * @param counterName Имя счетчика операций в сгенерированных кодах.
      * @param codes Исходные и сгенерированные коды алгоритма.
      */
-    public Algorithm(int id, String name, String mainMethod, String counterName, ArrayList<Code> codes) {
+    public Algorithm(long id, String name, String mainMethod, String counterName, ArrayList<Code> codes) {
         this.id = id;
         this.name = name;
         this.mainMethod = mainMethod;
@@ -40,4 +51,35 @@ public class Algorithm {
         this.codes = codes;
     }
 
+    /**
+     * Метод для задания id алгоритма в БД.
+     * @param id Индекс алгоритма в БД.
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Метод возвращает имя алгоритма.
+     * @return Имя алгоритма.
+     */
+    String getName() {
+        return this.name;
+    }
+
+    /**
+     * Метод возвращает метод вызова алгоритма.
+     * @return Метод вызова алгоритма.
+     */
+    String getMainMethod() {
+        return this.mainMethod;
+    }
+
+    /**
+     * Метод возвращает имя счетчика операций.
+     * @return Имя счетчика операций.
+     */
+    String getCounterName() {
+        return this.counterName;
+    }
 }
