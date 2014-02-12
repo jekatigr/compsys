6bbc7ba1fd4606b1ead372926097d48475b857eb
jekatigr <jekatigr@yandex.ru>
2014-02-12 22:34:43 +0400

    Добавлены getters & setters.

--- src/comparative_system/model/Code.java
+++ src/comparative_system/model/Code.java
@@ -12,7 +12,7 @@ package comparative_system.model;
  */
 public class Code {
     /** id кода в БД проекта. */
-    private int id;
+    private long id;
     /** Исходный код. */
     private String sourceCode;
     /** Код со вставленными счетчиками. */
@@ -20,11 +20,21 @@ public class Code {
     
     /**
      * Конструктор класса.
+     * @param sourceCode Исходный код.
+     * @param generatedCode Код со вставленными счетчиками.
+     */
+    public Code(String sourceCode, String generatedCode) {
+        this(-1, sourceCode, generatedCode);
+    }
+    
+    /**
+     * Конструктор класса.
      * @param id id кода в БД проекта.
      * @param sourceCode Исходный код.
      * @param generatedCode Код со вставленными счетчиками.
      */
-    public Code(int id, String sourceCode, String generatedCode) {
+    public Code(long id, String sourceCode, String generatedCode) {
+        this.id = id;
         this.sourceCode = sourceCode;
         this.generatedCode = generatedCode;
     }
@@ -43,5 +53,21 @@ public class Code {
      */
     public String getGeneratedCode() {
         return generatedCode;
+    }
+
+    /**
+     * Метод возвращает id исходных кодов в БД.
+     * @return id исходных кодов.
+     */
+    long getId() {
+        return this.id;
+    }
+
+    /**
+     * Метод для задания соответствующего БД id исходных кодов.
+     * @param id Индекс исходных кодов в БД.
+     */
+    void setId(long id) {
+        this.id = id;
     }
 }
