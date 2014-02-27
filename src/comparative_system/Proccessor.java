e98f63bf2762b8be485fd7d5a08b1e8d6672f3b1
jekatigr <jekatigr@yandex.ru>
2014-02-28 00:18:31 +0400

    Some changes without logic end. Need to be done later.

--- src/comparative_system/Proccessor.java
+++ src/comparative_system/Proccessor.java
@@ -8,27 +8,60 @@ package comparative_system;
 
 import comparative_system.model.Code;
 import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.Initializer;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 /**
- *
+ * 
  * @author TireX
  */
 public class Proccessor {
+    
+    static class Map {
+        private static class MapNode {
+            private int codeStart;
+            private int codeLength;
+            private ASTNode type;
+            private int countOfOperations;
+            public MapNode(int codeStart, int codeLength, ASTNode type, int countOfOperations) {
+                this.codeStart = codeStart;
+                this.codeLength = codeLength;
+                this.type = type;
+                this.countOfOperations = countOfOperations;
+            }
+        }
+        
+        ArrayList<MapNode> list; 
+        
+        public Map() {
+            list = new ArrayList<>();
+        }
+    }
+    
+    
+    
+    private static final ASTParser parser = ASTParser.newParser(AST.JLS3);
+    
     public static ArrayList<MethodDeclaration> getAllMethodsFromCodes(ArrayList<String> codes) {
         final ArrayList<MethodDeclaration> methods = new ArrayList<>();
         
         for(String code : codes) {
-            ASTParser parser = ASTParser.newParser(AST.JLS3);
             parser.setSource(code.toCharArray());
             parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
             final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
 
             cu.accept(new ASTVisitor() {
                     @Override
@@ -36,9 +69,6 @@ public class Proccessor {
                             if (!node.isConstructor()) {
                                 methods.add(node);
                             }
-                            
- //System.out.println("Declaration of method '" + node.getName() + "' at line " + cu.getLineNumber(node.getStartPosition()));
-
                             return true;
                     }
             });
@@ -51,7 +81,7 @@ public class Proccessor {
      * @param codes Исходные коды алгоритма.
      * @return Будущее имя счетчика.
      */
-    public static String getCounterName(ArrayList<String> codes) {//TODO: реализовать метод
+    public static String getCounterName(ArrayList<String> codes) {//TODO: реализовать метод getCounterName
         return "tempNameOfCounter";
     }
 
@@ -61,11 +91,51 @@ public class Proccessor {
      * @param codes Исходные коды алгоритма.
      * @return Коды с расставленными счетчиками.
      */
-    public static ArrayList<Code> putCounters(String counterName, ArrayList<String> codes) {//TODO: реализовать метод
+    public static ArrayList<Code> putCounters(String counterName, ArrayList<String> codes) {
         ArrayList<Code> gen_codes = new ArrayList<>();
-        for(int i = 0; i < codes.size(); i++) {
-            gen_codes.add(new Code(codes.get(i), codes.get(i)));
-        }
+        for (String code : codes) {
+            gen_codes.add(new Code(code, getGeneratedCode(counterName, code)));
+        }        
         return gen_codes;
     }   
+    
+    /**
+     * Метод возвращает строку, содержашую код с вставленными счетчиками.
+     * @param counterName Имя счетчика для вставки.
+     * @param code Исходный код одного public-класса.
+     * @return Код с вставленными счетчиками.
+     */
+    static String getGeneratedCode(String counterName, String code) {
+        Map map = buildNewMap(code);
+        return putCountersInCodeFromMap(map, counterName, code);
+    }
+    
+    static int countOperationsInExpression(Expression ex) {
+        return 0;       
+    }
+
+    static Map buildNewMap(String code) {
+        Map map = new Map();
+        
+        parser.setSource(code.toCharArray());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+        cu.accept(new ASTVisitor() {
+            //public boolean visit(TypeDeclaration node) {// заходим внутрь каждого класса, в т.ч. внутренних
+                //node.accept(new ASTVisitor() {
+                    public boolean visit(MethodDeclaration method) {// заходим внутрь каждого метода
+                        System.out.println("-- "+method.toString());
+                        return true;
+                //    }
+                //});
+                //return true;
+            }
+        });
+        return map;
+    }
+
+    static String putCountersInCodeFromMap(Map map, String counterName, String code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
