7d1c4997b1e2bc4e78229bdf07ec611eec925d7b
jekatigr <jekatigr@yandex.ru>
2014-03-04 19:26:45 +0400

    Изменения в методах подсчета операций (не закончены).

--- src/comparative_system/Proccessor.java
+++ src/comparative_system/Proccessor.java
@@ -2,6 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package comparative_system;
@@ -13,24 +17,35 @@ import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
 import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ContinueStatement;
+import org.eclipse.jdt.core.dom.EmptyStatement;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.LabeledStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.ParenthesizedExpression;
+import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
+import org.eclipse.jdt.core.dom.SuperMethodInvocation;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 /**
  * 
  * @author TireX
  */
 public class Proccessor {
-    private static AST AST;
     
     static class Map {
         private static class MapNode {
@@ -127,12 +142,26 @@ public class Proccessor {
         parser.setKind(ASTParser.K_COMPILATION_UNIT);
         final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
-        cu.accept(new ASTVisitor() {
-            public boolean visit(ExpressionStatement expression) {// ходим по всем действиям
-                map.addParsedExpression(expression.getStartPosition(), expression.getLength(), "Expression", countOperations(expression.getExpression()));
-                return true;
+        for (Object e : cu.types()) {        
+            if (e instanceof TypeDeclaration) {//если класс
+                for(MethodDeclaration m : ((TypeDeclaration)e).getMethods()) {                
+                    for (Object s : m.getBody().statements()) {                    
+                        System.out.println(s.toString()+"\n\n");
+                        countOperationsInStatement((Statement)s);
+                    }
+                }
             }
-        });
+        }
+        
+//        cu.accept(new ASTVisitor() {
+//            public boolean visit(BodyDeclaration node) {// ходим по всем действиям
+//                System.out.println();
+//                map.addParsedExpression(node.getStartPosition(), node.getLength(), "Expression", countOperations(node));
+//                System.out.println();
+//                System.out.println();
+//                return true;
+//            }
+//        });
         return map;
     }
 
@@ -141,15 +170,82 @@ public class Proccessor {
     }
     
     private static int count = 0;
-    static int countOperations(Expression ex) {
-        count = 0;
-        ex.accept(new ASTVisitor() {
-            public boolean visit(Assignment ass) {// ходим по всем выражениям с оператором присваивания
-                System.out.println("--" + ass.toString());
-                count++;
-                return false; // если вернуть true, то (h = s = 11) пройдет дважды с (s = 11)
+    static int countOperationsInExpression(ASTNode ex) {
+        if (ex instanceof MethodInvocation) {
+            for (Object e : ((MethodInvocation)ex).arguments()) 
+            {
+                countOperationsInExpression((Expression)e);
             }
-        });
+        }
+        if (ex instanceof SuperMethodInvocation) {
+            for (Object e : ((SuperMethodInvocation)ex).arguments()) 
+            {
+                countOperationsInExpression((Expression)e);
+            }
+        }
+        
+        if (ex instanceof VariableDeclarationExpression) {
+            for (Object e : ((VariableDeclarationExpression)ex).fragments()) 
+            {
+                countOperationsInExpression(((VariableDeclarationFragment)e).getInitializer());
+            }
+        }
+//        List list = ex.structuralPropertiesForType();
+//        for (int i = 0; i < list.size(); i++) {// в листе все дочерние узлы ex
+//            StructuralPropertyDescriptor curr = (StructuralPropertyDescriptor) list.get(i);
+//            Object child = ex.getStructuralProperty(curr);
+//            System.out.println(child.toString()+"    -    "+child.getClass().getName());
+//            if (child instanceof List) 
+//            {//Если внутри выражения есть еще одно
+//                for (Object e : (List)child) 
+//                {
+//                    countOperations((Expression)e); 
+//                }
+//            } 
+//            else 
+//            {   
+//                
+//            }
+                //if (child instanceof ASTNode) 
+                //{
+                    //System.out.println(child.toString()+"    -    "+child.getClass().getName());
+                    //return new Object[] { child };
+                //}
+            //return new Object[0];
+//        }
+        
+        
+//        ex.accept(new ASTVisitor() {
+//            public boolean visit(Assignment ass) {// ходим по всем выражениям с оператором присваивания
+//                countOperations(ass.getRightHandSide());
+//                return false; // если вернуть true, то (h = s = 11) пройдет дважды с (s = 11)
+//            }
+//        });
         return count;
+    }
+    
+    private static void countOperationsInStatement(Statement st) {
+        
+        if (st instanceof EmptyStatement || st instanceof ContinueStatement || st instanceof BreakStatement ) {
+            return;
+        }
+        
+        if (st instanceof ExpressionStatement) {
+            countOperationsInExpression(((ExpressionStatement)st).getExpression());
+        }
+        
+        if (st instanceof LabeledStatement) {
+            countOperationsInStatement(((LabeledStatement)st).getBody());
+        }
+        
+        if (st instanceof Block) {
+            for (Object s : ((Block)st).statements()) {
+                countOperationsInStatement((Statement)s);
+            }
+        }
+        
+        if (st instanceof ReturnStatement) {
+            countOperationsInExpression(((ReturnStatement)st).getExpression());
+        }
     }
 }
