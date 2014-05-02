45580ca05db786749834d54a10b89145f1bbc4f8
jekatigr <jekatigr@yandex.ru>
2014-05-02 05:39:26 +0400

    Добавлен класс результатов.

---
+++ src/comparative_system/model/Result.java
@@ -0,0 +1,22 @@
+package comparative_system.model;
+
+/**
+ * Класс, реализующий результат вычислений трудоевкости для алгоритма.
+ * @author Gromov Evg.
+ */
+public class Result {
+    /** id набора данных в БД проекта. */
+    long data_id;
+    /** Количество операций. */
+    int count;
+    
+    /**
+     * Конструктор класса.
+     * @param data_id id набора данных.
+     * @param count Количество операций.
+     */
+    public Result(long data_id, int count) {
+        this.data_id = data_id;
+        this.count = count;
+    }
+}
